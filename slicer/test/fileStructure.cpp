ce46b82d0323fc54c387fb2e3c8c6f1813834189
Dan Goodliffe <dan@randomdan.homeip.net>
2015-07-20 23:26:02 +0100

    Switch to run rule for capturing output, undate paths in tests

--- slicer/test/fileStructure.cpp
+++ slicer/test/fileStructure.cpp
@@ -2,11 +2,15 @@
 #include <boost/test/unit_test.hpp>
 #include <boost/filesystem/convenience.hpp>
 
+#define XSTR(s) STR(s)
+#define STR(s) #s
+const boost::filesystem::path root(XSTR(ROOT));
+
 FileStructure::FileStructure() :
 	me(boost::filesystem::canonical("/proc/self/exe")),
 	base("types"),
-	bjamout(me.parent_path().parent_path().parent_path().leaf() / me.parent_path().parent_path().leaf() / me.parent_path().leaf()),
-	root(me.parent_path().parent_path().parent_path().parent_path()),
+	bjamout("bin" / me.parent_path().parent_path().leaf() / me.parent_path().leaf()),
+	root(::root),
 	included(root / "included"),
 	slice(fs::change_extension(root / base, ".ice")),
 	tmp(root / "bin" / "slicer")
