4f40932a6b2eac8f8e21f841289e1b710c51bd65
Dan Goodliffe <dan@randomdan.homeip.net>
2015-10-15 22:14:31 +0100

    Use rvalue refs for serializer wrappers (sub-slot change required)

--- slicer/slicer/slicer.h
+++ slicer/slicer/slicer.h
@@ -8,7 +8,7 @@
 namespace Slicer {
 	template <typename Deserializer, typename Object, typename ... SerializerParams>
 	Object
-	DeserializeAny(SerializerParams & ... sp)
+	DeserializeAny(SerializerParams && ... sp)
 	{
 		IceUtil::Handle<ModelPartForRoot<Object>> root = new ModelPartForRoot<Object>();
 		DeserializerPtr deserializer = new Deserializer(sp ...);
@@ -18,14 +18,14 @@ namespace Slicer {
 
 	template <typename Deserializer, typename Object, typename ... SerializerParams>
 	IceInternal::Handle<Object>
-	Deserialize(SerializerParams & ... sp)
+	Deserialize(SerializerParams && ... sp)
 	{
 		return DeserializeAny<Deserializer, IceInternal::Handle<Object>, SerializerParams...>(sp ...);
 	}
 
 	template <typename Serializer, typename Object, typename ... SerializerParams>
 	void
-	SerializeAny(Object object, SerializerParams & ... sp)
+	SerializeAny(Object object, SerializerParams && ... sp)
 	{
 		IceUtil::Handle<ModelPartForRoot<Object>> root = new ModelPartForRoot<Object>(object);
 		SerializerPtr serializer = new Serializer(sp ...);
@@ -34,7 +34,7 @@ namespace Slicer {
 
 	template <typename Serializer, typename Object, typename ... SerializerParams>
 	void
-	Serialize(IceInternal::Handle<Object> object, SerializerParams & ... sp)
+	Serialize(IceInternal::Handle<Object> object, SerializerParams && ... sp)
 	{
 		SerializeAny<Serializer>(object, sp ...);
 	}
