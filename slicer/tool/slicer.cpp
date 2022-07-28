a42f896351509219c8c9df0d6368f84b77a9a0d3
Dan Goodliffe <dan@randomdan.homeip.net>
2022-07-28 02:39:15 +0100

    Don't pass potentially NULL PATH env to string_view

--- slicer/tool/slicer.cpp
+++ slicer/tool/slicer.cpp
@@ -29,7 +29,12 @@ defaultPostProcessor()
 	constexpr std::array<const std::pair<std::string_view, std::string_view>, 1> pps {{
 			{"clang-format", "-i"},
 	}};
-	const std::string_view path {getenv("PATH")};
+	const auto path {[]() -> std::string_view {
+		if (auto p = getenv("PATH")) {
+			return p;
+		}
+		return "";
+	}()};
 	const auto pathBegin = make_split_iterator(path, first_finder(":", boost::is_equal()));
 	for (const auto & [cmd, opts] : pps) {
 		for (auto p = pathBegin; p != decltype(pathBegin) {}; ++p) {
